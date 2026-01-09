9cb74523680614e0dec4d3340ef6e20691543fb2
Shivash Jurakan <ShivashJ@mintek.co.za>
2026-01-09 11:36:34 +0200

    Add Job entity

--- src/main/java/com/shivash/job_app_tracker/model/Job.java
+++ src/main/java/com/shivash/job_app_tracker/model/Job.java
@@ -1,19 +1,82 @@
 package com.shivash.job_app_tracker.model;
 
 import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Entity
+@Table(name="jobs")
 public class Job {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Long id;
+    private Long id;
 
+    @Column(nullable = false)
     String company;
+
+    @Column(nullable = false)
     String role;
-    LocalDateTime dateApplied;
-    @Enumerated(EnumType.STRING)
-    Status status;
+
+    @Column(nullable = false)
+    LocalDate dateApplied;
+
+    @Column(nullable = false)
+    String status;
+
+    @Column(length = 1000)
     String notes;
+
+    public Job() {
+    }
+
+    public Job(String company, String role, LocalDate dateApplied, String status, String notes) {
+        this.company = company;
+        this.role = role;
+        this.dateApplied = dateApplied;
+        this.status = status;
+        this.notes = notes;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setDateApplied(LocalDate dateApplied) {
+        this.dateApplied = dateApplied;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public LocalDate getDateApplied() {
+        return dateApplied;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
 }
--- src/main/java/com/shivash/job_app_tracker/model/Status.java
+++
@@ -1,7 +0,0 @@
-package com.shivash.job_app_tracker.model;
-
-public enum Status {
-    APPLIED,
-    PENDING,
-    REJECTED
-}
